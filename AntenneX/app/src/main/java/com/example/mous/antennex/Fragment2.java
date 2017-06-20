a936adad094b9a98c657c255d58bee8739fb4309
Mastafa Foufa <mastafa.foufa@hotmail.com>
2017-06-20 15:57:22 +0200

    Keep on change

--- AntenneX/app/src/main/java/com/example/mous/antennex/Fragment2.java
+++ AntenneX/app/src/main/java/com/example/mous/antennex/Fragment2.java
@@ -13,20 +13,43 @@ public class Fragment2 extends Fragment implements View.OnClickListener{
 
     Button gallery_btn ;
 
+    Button resume_btn;
+
+    Button carte_btn;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedinstanceState) {
         View myView = inflater.inflate(R.layout.fragment2_layout, container, false);
         gallery_btn = (Button) myView.findViewById(R.id.gallery);
         gallery_btn.setOnClickListener(this);
+        resume_btn = (Button) myView.findViewById(R.id.resume_btn);
+        resume_btn.setOnClickListener(this);
+        carte_btn = (Button) myView.findViewById(R.id.carte_btn);
+        carte_btn.setOnClickListener(this);
+
+
         return myView;
     }
 
     @Override
     public void onClick(View v) {
 
-        Intent intent = new Intent (getActivity(), GalleryActivity.class);
-        getActivity().startActivity(intent);
+        switch (v.getId()) {
+            case R.id.gallery:
+                Intent intent = new Intent (getActivity(), GalleryActivity.class);
+                getActivity().startActivity(intent);
+                break;
+            case R.id.resume_btn:
+                Intent intent2 = new Intent (getActivity(), ResumeActivity.class);
+                getActivity().startActivity(intent2);
+                break;
+            case R.id.carte_btn:
+                Intent intent3 = new Intent (getActivity(), ResumeActivity.class);
+                getActivity().startActivity(intent3);
+                break;
+        }
+
 
     }
 
