a2fc95a1b398f04211eb6f8be866b0fba9870c94
Bolong <bolong.zhang@telecom-paristech.fr>
2017-06-20 10:22:05 +0200

    vérification de l'adresse autour de Télecom Paristech

--- client_server/app/src/test/java/com/paf/bolong/client_server/AppRequestTest.java
+++ client_server/app/src/test/java/com/paf/bolong/client_server/AppRequestTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 public class AppRequestTest {
     @Test
     public void json() throws Exception {
-        System.out.print(AppRequest.json("48.8520930694", "2.34738897095", "100"));
+        System.out.print(AppRequest.json("48.82628", "2.34519", "100"));
     }
 }
 
