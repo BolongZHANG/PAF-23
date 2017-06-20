987de5685fd9821eef5adc3507de05c60f6f402b
Arthur Garnier <arthur.g5@club-internet.fr>
2017-06-20 18:16:52 +0200

    Code avec deux type de map pour afficher la map des antennes en 2D

--- GPSPAFPAF.py
+++ GPSPAFPAF.py
@@ -18,7 +18,7 @@ import folium
 from folium.features import CustomIcon
 
 #on fabrique notre propre pointeur en forme de chat 
-icon_image = '/Users/arthurbg/Desktop/Unknown.png'
+icon_image = '/Users/arthurbg/Desktop/anaconda/lib/python3.5/site-packages/gmplot/markers/logo.png'
 
 icon = CustomIcon(
     icon_image,
@@ -26,6 +26,23 @@ icon = CustomIcon(
     icon_anchor=(10, 40)
 )
 
+map_osm = folium.Map(location=[46.44, 0.12])
+#HTML('map_osm')
+#from pyensae import folium_html_map
+for i,point in enumerate(points[1:100]):
+   #print(points[1].speed_between(points[2]))
+   #print('Point at ({0},{1},{2}) : {3}'.format(point.latitude, point.longitude, point.elevation,point.time))
+
+   #map_osm.add_child(folium.CircleMarker(location=[point.latitude, point.longitude], popup='a',fill_color='red', radius=2))
+   #map_osm.add_child(folium.CircleMarker(location=[point.latitude, point.longitude], popup='a',fill_color='blue', radius=2))
+   if (i == len(points[1:100]) - 1):
+       #map_osm.simple_marker([point.latitude, point.longitude])
+       #folium.Marker([point.latitude, point.longitude], popup='votre matou est ici',icon = folium.Icon(color ='green')).add_to(map_osm)
+       marker = folium.Marker(location=[point.latitude, point.longitude], icon=icon, popup='Antenne bouygues telecom ')
+       map_osm.add_child(marker)
+
+
+map_osm.save('o.html')
 ## on demande à l'utilisateur de donner une limite et les coordonées gps de la référence
 
 
@@ -51,6 +68,6 @@ import gmplot
 gmap = gmplot.GoogleMapPlotter(46.439882,0.11577,16)
 
 
-gmap.scatter([46.43991,46.43981,46.43971,46.43961], [0.115835,0.115845,0.115855,0.115865], '#FFFAFA',size = 1, marker=True)
+gmap.scatter([46.43991,46.43981,46.43971,46.43961], [0.115835,0.115845,0.115855,0.115865], '#logo2',size = 1, marker=True)
 
-gmap.draw("mymappaf.html")
+gmap.draw("test34.html")
