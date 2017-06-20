e1a28d1b4ab577e4e476e113c6dcd5cbb4eecca6
Mastafa Foufa <mastafa.foufa@hotmail.com>
2017-06-20 14:43:45 +0200

    Little changes

--- AntenneX/app/src/main/java/com/example/mous/antennex/Fragment2.java
+++ AntenneX/app/src/main/java/com/example/mous/antennex/Fragment2.java
@@ -1,12 +1,36 @@
 package com.example.mous.antennex;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-public class Fragment2 extends Fragment {
+public class Fragment2 extends Fragment implements View.OnClickListener{
+
+    Button gallery_btn ;
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedinstanceState) {
+        View myView = inflater.inflate(R.layout.fragment2_layout, container, false);
+        gallery_btn = (Button) myView.findViewById(R.id.gallery);
+        gallery_btn.setOnClickListener(this);
+        return myView;
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        Intent intent = new Intent (getActivity(), GalleryActivity.class);
+        getActivity().startActivity(intent);
+
+    }
+
+
     public static Fragment2 newInstance() {
         Fragment2 fragment = new Fragment2();
         return fragment;
@@ -17,9 +41,5 @@ public class Fragment2 extends Fragment {
         super.onCreate(savedInstanceState);
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment2_layout, container, false);
-    }
+
 }
