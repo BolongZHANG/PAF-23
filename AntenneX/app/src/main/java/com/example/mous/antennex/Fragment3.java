8165cab54e369985ccbcad8fc2f955d935918bef
Samuel <samuel.leonardo@telecom-paristech.fr>
2017-06-23 17:53:20 +0200

    ça marche pas mais touchez pas, c'est pour moi, n'en déplaise à certains

--- AntenneX/app/src/main/java/com/example/mous/antennex/Fragment3.java
+++ AntenneX/app/src/main/java/com/example/mous/antennex/Fragment3.java
@@ -1,6 +1,9 @@
 package com.example.mous.antennex;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatDelegate;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,15 +17,42 @@ public class Fragment3 extends Fragment {
         return fragment;
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+
+    public static class MainActivity extends AppCompatActivity {
+
+        static {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO);
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main_test);
+
+            if (savedInstanceState == null) {
+                Fragment preferenceFragment = new PreferenceFragmentCustom();
+                FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                ft.add(R.id.pref_container, preferenceFragment);
+                ft.commit();
+            }
+
+        }
+
+
     }
+
+
+
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment3_layout, container, false);
+        return inflater.inflate(R.layout.activity_main_test, container, false);
     }
 }
 
