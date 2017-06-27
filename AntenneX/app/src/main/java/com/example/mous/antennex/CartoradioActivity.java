dea3796c89395efe6648561a0bfaf91fe55f6797
Bolong <bolong.zhang@telecom-paristech.fr>
2017-06-27 20:10:31 +0200

    Cartoradio avec position GPS et zoomer !!!

--- AntenneX/app/src/main/java/com/example/mous/antennex/CartoradioActivity.java
+++ AntenneX/app/src/main/java/com/example/mous/antennex/CartoradioActivity.java
@@ -1,45 +1,24 @@
 package com.example.mous.antennex;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.Environment;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
-import android.widget.Toast;
+import android.util.Log;
+
 
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.ObjectOutputStream;
-import java.io.OutputStream;
-import java.net.Socket;
-
-import android.content.Context;
-import android.content.Intent;
-import android.os.Bundle;
-import android.os.Environment;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.webkit.GeolocationPermissions;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.ObjectOutputStream;
-import java.io.OutputStream;
-import java.net.Socket;
-import java.util.Random;
+
+import static com.example.mous.antennex.R.id.webView;
 
 public class CartoradioActivity extends AppCompatActivity {
 
@@ -54,10 +33,41 @@ public class CartoradioActivity extends AppCompatActivity {
         setContentView(R.layout.activity_cartoradio);
 
 
-        WebView myWebView = (WebView) findViewById(R.id.webView);
+        WebView myWebView = (WebView) findViewById(webView);
         myWebView.setWebViewClient(new WebViewClient());
         WebSettings webSettings = myWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
+        webSettings.setGeolocationEnabled(true);
+        myWebView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onGeolocationPermissionsHidePrompt() {
+                super.onGeolocationPermissionsHidePrompt();
+                Log.i("geo", "onGeolocationPermissionsHidePrompt");
+            }
+
+            @Override
+            public void onGeolocationPermissionsShowPrompt(final String origin,
+                                                           final GeolocationPermissions.Callback callback) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(CartoradioActivity.this);
+                builder.setMessage("Allow to access location information?");
+                DialogInterface.OnClickListener dialogButtonOnClickListener = new DialogInterface.OnClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface dialog, int clickedButton) {
+                        if (DialogInterface.BUTTON_POSITIVE == clickedButton) {
+                            callback.invoke(origin, true, true);
+                        } else if (DialogInterface.BUTTON_NEGATIVE == clickedButton) {
+                            callback.invoke(origin, false, false);
+                        }
+                    }
+                };
+                builder.setPositiveButton("Allow", dialogButtonOnClickListener);
+                builder.setNegativeButton("Deny", dialogButtonOnClickListener);
+                builder.show();
+                super.onGeolocationPermissionsShowPrompt(origin, callback);
+                Log.i("geo", "onGeolocationPermissionsShowPrompt");
+            }
+        });
         myWebView.loadUrl("file:///android_asset/Cartoradio.html");
 
     }
